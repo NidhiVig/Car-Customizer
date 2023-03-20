6196795f1fdb66a1ad7d9b4e012710b1a8a767e8
NidhiVig.github <nidhivig27@gmail.com>
2023-03-20 10:35:43 +0530

    Final

---
+++ Assets/Scripts/Changecolor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changecolor : MonoBehaviour
+{
+    public Color[] colors;
+    public Renderer[] mats;
+    public void Red() {
+        for (int i = 0; i < mats.Length; i++) {
+            mats[i].material.color = colors[0];
+        }
+    }
+    public void Blue() {
+        for (int i = 0; i < mats.Length; i++) {
+            mats[i].material.color = colors[2];
+        }
+    }
+    public void Black() {
+        for (int i = 0; i < mats.Length; i++) {
+            mats[i].material.color = colors[1];
+        }
+    }
+    public void Yellow() {
+        for (int i = 0; i < mats.Length; i++) {
+            mats[i].material.color = colors[3];
+        }
+    }
+}
